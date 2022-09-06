7c059e8c349b35a36ba6512bae33358df59f4061
Vishnu <cv.vishnuvardhan.reddy@gmail.com>
2022-09-06 12:36:51 +0530

    second commit

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.get('/', (req,res) => {
     res.send('Welcome');
 })
 
+app.get('/first-get', (req,res) => {
+    res.status(200).send({
+        success: true
+    })
+})
+
 app.listen(8008, () => {
     console.log('Server is running')
 })
